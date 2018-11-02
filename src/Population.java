d09daa88e38eae5fa66b616704e156517fa64987
timothy_kwan <timothy_kwan@student.wayland.k12.ma.us>
2018-11-02 09:02:49 -0400

    get score

--- src/Population.java
+++ src/Population.java
@@ -1,9 +1,21 @@
 import java.util.ArrayList;
 
 public class Population {
-    TestCities tc=new TestCities();
+    TestCities tc = new TestCities();
     ArrayList<City> cities = tc.getCities();
-    ArrayList<City>[] population = new ArrayList<City>[40];
+    ArrayList<City>[] population = new ArrayList[20];
+
+    public int getScore(ArrayList<City> cit){
+        int sum = 0;
+        for (int i = 0; i < cit.size() - 1; i++) {
+            int xLength = Math.abs(cit.get(i).getX() - cit.get(i + 1).getX());
+            int yLength = Math.abs(cit.get(i).getX() - cit.get(i + 1).getX());
+
+            sum += Math.sqrt(xLength * xLength + yLength * yLength);
+        }
+
+        return sum;
+    }
 
 
 }
