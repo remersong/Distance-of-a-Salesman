57b0f6ce23fa47501df7418faa11287321bc4d4e
timothy_kwan <timothy_kwan@student.wayland.k12.ma.us>
2018-11-02 09:06:57 -0400

    Merged

--- src/Population.java
+++ src/Population.java
@@ -45,7 +45,18 @@ public class Population {
             return containsAll;
         }
 
-//
+    public int getScore(ArrayList<City> cit){
+        int sum = 0;
+        for (int i = 0; i < cit.size() - 1; i++) {
+            int xLength = Math.abs(cit.get(i).getX() - cit.get(i + 1).getX());
+            int yLength = Math.abs(cit.get(i).getX() - cit.get(i + 1).getX());
+
+            sum += Math.sqrt(xLength * xLength + yLength * yLength);
+        }
+
+        return sum;
+    }
+
 
     }
 
