7ae494c9c1da5ebbea45b2a9b82068a5fa60d95e
robert_glazer <remersong@gmail.com>
2018-11-05 08:15:08 -0500

    timer

--- src/WindowPanel.java
+++ src/WindowPanel.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class WindowPanel extends JPanel {
+    int gen=1;
     public WindowPanel(int w, int h) {
         setSize(w, h);
     }
@@ -14,12 +15,25 @@ public class WindowPanel extends JPanel {
 
 
     public void paintComponent(Graphics g){
+        Graphics2D g2 = (Graphics2D)g;
+
         Population population=new Population();
         population.createPopulation();
+        Timer timer = new Timer(1000 / 600, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //called 60 times per second
+                population.display();
+                //
+                population.Update();
+                gen++;
+                repaint();
+            }
+
+        });
+        timer.start();
         population.display();
-        population.Update();
-        population.display();
-        Graphics2D g2 = (Graphics2D)g;
+        g2.drawString((Integer.toString(gen)), 100, 100);
         g2.drawRect(5, 5, 5, 5);
         //putting timer (move method) in here accelerates it. Why? Not sure
     }
